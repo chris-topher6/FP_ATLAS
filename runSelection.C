c0fdd9d0b1132b82d6f1d382c019d1c7d12b0c12
Christopher Breitfeld <christopher.breitfeld@tu-dortmund.de>
2023-05-08 18:13:55 +0200

    läuft für 1 Datei ganz gut, aber nicht mit Loop

--- runSelection.C
+++ runSelection.C
@@ -2,6 +2,8 @@
 #include "fileHelper.h"
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <fstream>
 
 using namespace std;
 
@@ -14,6 +16,7 @@ int main(int argn, char *argv[]) {
   }
   // path to the file to be studied + filename from argument called when executing file
   string inpath = string(argv[1]);
+//  std::filesystem::path inputPath = std::filesystem::path(inpath); //GPT
   TString filename = TString(inpath).ReplaceAll("/net/e4-nfs-home.e4.physik.tu-dortmund.de/home/zprime/E4/Final/samples/", "");
 
   cout << "Processing " << filename << endl;
@@ -34,16 +37,30 @@ int main(int argn, char *argv[]) {
   }
 
   // create file to which the selection is going to be saved to
-  TString outpath = "output_runSelection/" + filename;
+//  TString outpath = "output_runSelection/" + TString(filename);
+  TString outpath = "output_runSelection/data.el_selected.root"; //!!! das hier ist nicht elegant, aber ich habe keinen anderen Weg gefunden. Muss noch geändert werden!!!
   outpath.ReplaceAll(".root", "_selected.root");
   TFile * newFile = new TFile(outpath, "RECREATE");
+
+  cout << "Output path: " << outpath << endl;
+  cout << "Filename: " << filename << endl;
+
   
   // make a copy of the tree to be saved after selection
   TTree * newTree = tree->CloneTree();
-    
-   
+
+  // Define parameters for efficiency
+  int totalEvents=0;
+  int passedEvents;
+  int count_lep_n=0;  
+  int count_jet_n=0;
+  int count_jet_good=0; 
+  int count_bjet=0;
+
   // now loop over the events in the tree
   for (int iEntry = 0; iEntry < nEntries; ++iEntry) {
+    
+    totalEvents++;
 
     // get entry no. iEntry and tell every 100000th event
     tree->GetEntry(iEntry);
@@ -53,20 +70,61 @@ int main(int argn, char *argv[]) {
 
     //////////////////////////////////////////////////////
     // To do: Implement all required selection criteria //
-    //////////////////////////////////////////////////////
-
-
     bool passCriteria = true;
+    if (tree->lep_n != 1) {
+      count_lep_n++;
+      passCriteria = false;
+    }
+    if (tree->jet_n != 4) {
+      count_jet_n++;
+      passCriteria = false;
+    }
+    if (*tree->jet_good != 1) {
+      count_jet_good++;
+      passCriteria = false;
+    }
 
-
-
-
+    int bJetCount = 0;
+    for (int iJet = 0; iJet < static_cast<int>(tree->jet_n); ++iJet) {
+      if (tree->jet_MV1[iJet] > 0.78) {
+        bJetCount++;
+      }
+    }
+    if (bJetCount != 2) {
+      count_bjet++;
+      passCriteria = false;
+    }
+    //////////////////////////////////////////////////////
+  
     // check all selection criteria and only save the event to the new
     // tree if all of them are true
     if (passCriteria) {
+      passedEvents++;
       newTree->Fill();
     }
-    
+
+  }
+
+  // efficiencies
+  double eff_lep_n    = static_cast<double>(totalEvents-count_lep_n)   / totalEvents;
+  double eff_jet_n    = static_cast<double>(totalEvents-count_jet_n)   / totalEvents;
+  double eff_jet_good = static_cast<double>(totalEvents-count_jet_good)/ totalEvents;
+  double eff_bjet = static_cast<double>(totalEvents-count_bjet)/ totalEvents;
+
+
+  // save efficiencies in .txt
+  ofstream outputFile("output_runSelection/efficiencies.txt");
+  if (outputFile.is_open()) {
+    outputFile << "Efficiency lep_n: " << eff_lep_n << endl;
+    outputFile << "Efficiency jet_n: " << eff_jet_n << endl;
+    outputFile << "Efficiency jet_good: " << eff_jet_good << endl;
+    outputFile << "Efficiency bjet:" << eff_bjet << endl;
+
+    outputFile.close();
+    cout << "Efficiencies saved to efficiencies.txt" << endl;
+    }
+  else {
+    cout << "Unable to open file for writing" << endl;
   }
 
   // save new tree to file
